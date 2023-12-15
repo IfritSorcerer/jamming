5e2e6be7c4ce1c31c8057180f2b24aa3f2359e96
ColemanSWE <coleman@firetail.io>
2023-12-15 18:28:57 +0200

    Initial review.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import SearchBar from '../SearchBar/SeachBar';
-import SearchResults from '../SearchBar/SearchResults';
-import Playlist from '../Playlist/Playlist';
-import Tracklist from '../Track/Tracklist';
-import Track from '../Track/Track';
+import React from "react";
+import SearchBar from "../SearchBar/SeachBar";
+import SearchResults from "../SearchBar/SearchResults";
+import Playlist from "../Playlist/Playlist";
+import Tracklist from "../Track/Tracklist";
+import Track from "../Track/Track";
+import trackId from "../Track/Track";
 
-
-function App(){
-    return (
-    <div> 
-        <h1>Jamming!</h1>
-        <SearchBar />
-        <SearchResults />
-        <Tracklist />
-        <Playlist />
-    </div> 
- )
-};
+function App() {
+  return (
+    <div>
+      <h1>Jamming!</h1>
+      <SearchBar />
+      <SearchResults />
+      <Tracklist trackId={trackId} />{" "}
+      {/* To go along with the change I made in the tracklist file and give you an example of props and destructuring. */}
+      <Playlist />
+    </div>
+  );
+}
 
 export default App;
--- src/components/SearchBar/SearchResults.js
+++ src/components/SearchBar/SearchResults.js
@@ -1,10 +1,24 @@
 import React from "react";
 
+// One thing that might work is for this to become a container for stuff.
+// Like a thing containing tracklist or playlist for example.
 
-function SearchResults() {
-    return (
-        <h2>Results:</h2>
-    )
+// also another pro tip, instead of using (props)
+// you can do something like ({ children }) where
+// children is just the name of the prop that you
+// pass to the component. This is called object destructuring
+// and is just another "advanced" thing https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+// long story short it just is like this:
+// const name = person.name
+// const { name } = person
+// these two lines mean the same thing ✅
+const SearchResults = (props) => {
+  return (
+    <>
+      <h2>Results:</h2>
+      {props.children} {/* could also turn this into */}
+    </>
+  );
 };
 
 export default SearchResults;
